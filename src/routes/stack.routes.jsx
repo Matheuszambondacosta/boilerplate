8fb55154dcf34abbbb57f8fd7a1801e096eff46e
Matheszambondacosta <matheus-costa@aluno.senai.br>
2024-03-13 08:48:24 -0300

    Adiciona telas de perfil e categoria

--- src/routes/stack.routes.jsx
+++ src/routes/stack.routes.jsx
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "../screens/Home";
+import Profile from "../screens/Profile";
+import Category from "../screens/Category";
 
 
 
@@ -10,7 +12,13 @@ const StackRoutes = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Category" component={Category} />
     </Stack.Navigator>
+
+
+
+
   );
 };
 
