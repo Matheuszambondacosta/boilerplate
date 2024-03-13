fd3670bdcf492914594ca4554f5192814a0790b3
Matheszambondacosta <matheus-costa@aluno.senai.br>
2024-03-13 09:13:01 -0300

    Atualizações nas rotas de navegação

--- src/routes/stack.routes.jsx
+++ src/routes/stack.routes.jsx
@@ -1,8 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "../screens/Home";
-import Profile from "../screens/Profile";
-import Category from "../screens/Category";
+
 
 
 
@@ -12,13 +11,7 @@ const StackRoutes = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Category" component={Category} />
     </Stack.Navigator>
-
-
-
-
   );
 };
 
